74fb482f15cfa79d572febcd5c9f7abfa46d2504
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2024-08-28 17:59:05 +0530

    revision

--- scope.js
+++ scope.js
@@ -1,6 +1,6 @@
 /* 
 1. ⁡⁣⁣⁢Global Scope⁡ ⁡⁢⁣⁢=>⁡ Variables declared outside of the function have Global Scope and accessable across all the code. Variables declared in Global Scope remains in the memory which can result in "out of memory" like problem.
-2. ⁡⁣⁣⁢Local or Function Scope⁡ ⁡⁢⁣⁢=>⁡ Variables declared inside of the function have Local or Function Scope and only accessable inside that function. Variables declared in Local Scope is deleted from the memory as soon as the function is executed.
+2. ⁡⁣⁣⁢Local or Function Scope⁡ ⁡⁢⁣⁢=>⁡ Variables declared inside of the function have Local or Function Scope and only accessable inside that function. Variables declared in Local Scope gets deleted from the memory as soon as the function is executed.
 3. ⁡⁣⁣⁢Block level Scope⁡ ⁡⁢⁣⁢=>⁡ Variable "Const" or "Let" have Block level scope means they are accessable only in the block of code where they're declared but it's not the case with "Var" variables (they're accessable from global scope). 
 */
 
